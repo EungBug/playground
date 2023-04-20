b6301266573784862c4c2c889b68ecb77019e920
EungBug <eungb14@gmail.com>
2023-04-21 00:29:03 +0900

    Study JS Movie Search - Component Class & Event Handle

---
+++ js-movie-search/src/App.js
@@ -0,0 +1,29 @@
+import { Component } from './core/eung';
+
+export default class App extends Component {
+  // 기본값 사용 시 생략 가능
+  constructor() {
+    super({
+      state: {
+        inputText: ''
+      }
+    });
+  }
+  render() {
+    this.el.classList.add('search');
+    this.el.innerHTML = /*html*/ `
+      <input />
+      <button>Click!</button>
+    `;
+
+    const inputEl = this.el.querySelector('input');
+    inputEl.addEventListener('input', () => {
+      this.state.inputText = inputEl.value;
+    });
+
+    const buttonEl = this.el.querySelector('button');
+    buttonEl.addEventListener('click', () => {
+      console.log(this.state.inputText);
+    });
+  }
+}
