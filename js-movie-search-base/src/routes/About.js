301b3694a20cacd2def40d67bbe4ad145c2b8a87
EungBug <eungb14@gmail.com>
2023-04-24 01:14:38 +0900

    Delete

--- js-movie-search-base/src/routes/About.js
+++ js-movie-search-base/src/routes/About.js
@@ -2,6 +2,7 @@ import { Component } from '../core/eung';
 
 export default class About extends Component {
   render() {
+    const {} = history.state;
     this.el.innerHTML =
       /* html */
       `
--- js-movie-search/src/store/message.js
+++
@@ -1,5 +0,0 @@
-import { Store } from '../core/eung';
-
-export default new Store({
-  message: 'Hello~'
-});
