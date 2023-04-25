62a9b06208e98f6d94c244ba6161322b255b70b4
EungBug <eungb14@gmail.com>
2023-04-25 15:06:59 +0900

    Feature Movie Details Info View

--- movie-search-app/src/routes/index.js
+++ movie-search-app/src/routes/index.js
@@ -1,9 +1,14 @@
 import { createRouter } from '../core/eungb';
 import Home from './Home';
+import Movie from './Movie';
 
 export default createRouter([
   {
     path: '#/',
     component: Home
+  },
+  {
+    path: '#/movie',
+    component: Movie
   }
 ]);
